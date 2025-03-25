c5c4433802cc5d12cbf19c7d9dc92cb393303416
Dave7c7 <dave7c7@gmail.com>
2025-03-25 15:54:35 -0400

    prefinal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 let btn = document.getElementById("btn-pwd");
@@ -6,6 +6,15 @@ let pwd_one = document.getElementById("passwords-one");
 let pwd_two = document.getElementById("passwords-two");
 let length = 0;
 
+//checkbox cheacked
+let checkbox = document.getElementById("symbols");
+    checkbox.addEventListener("change", () => {
+        if(checkbox.checked){
+            console.log("checked box works!");
+            const letters = characters.filter(char => /[a-zA-Z]/.test(char)); //return only letters
+            characters = letters
+        }
+    })
 
 function generate(){
     length = document.getElementById("userInput").value;
